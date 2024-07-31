f8cc37324052b134ddc65f3447e82a036f629bd9
J0rge08 <suarezinfantejorgemanuel@gmail.com>
2024-07-31 11:32:13 -0500

    Create J0rge08.java

---
+++ Roadmap/00 - SINTAXIS, VARIABLES, TIPOS DE DATOS Y HOLA MUNDO/java/J0rge08.java
@@ -0,0 +1,32 @@
+/*
+
+ * EJERCICIO:
+ * ¿Fácil? No te preocupes, recuerda que esta es una ruta de estudio y
+ * debemos comenzar por el principio.
+ */
+ //Crea un comentario en el código y coloca la URL del sitio web oficial del
+ //lenguaje de programación que has seleccionado.
+
+//https://www.java.com/es/
+ // Representa las diferentes sintaxis que existen de crear comentarios en el lenguaje (en una línea, varias...).
+
+/*cometario en java*/
+/**
+*esto tambien es un 
+*cometario
+*de
+*java
+*
+*/
+// Crea una variable (y una constante si el lenguaje lo soporta).
+   private variable = String;
+    int num = 2;
+// Crea variables representando todos los tipos de datos primitivos del lenguaje (cadenas de texto, enteros, booleanos...)
+  int diez = 10;
+  float decimal = 1.4;
+  double decimalGrande = 1.4536323425;
+  char numero = 1;
+  boolean EstasAprendiendo = true;
+// Imprime por terminal el texto: "¡Hola, [y el nombre de tu lenguaje]!"
+  public static void main(String[] args);
+    System.out.println ("Hola mundo");
